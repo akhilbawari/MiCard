6e2be5385cc12554c159c4a0e6d55097a02b4f19
akhilbawari <akhilbawari0804@gmail.com>
2024-02-01 23:21:13 +0530

    added project

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MiApp());
+}
+
+class MiApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blue,
+      body: Container(),
+    ),
+  );
+  }
+}
+
